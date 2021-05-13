1367f9b37bd060aed84b608db6add553b0ba300f
Pedro Neri <pedneri1@gmail.com>
2021-05-12 22:03:09 -0300

    Add formatter

--- lib/codecovPhoenixExample_web.ex
+++ lib/codecovPhoenixExample_web.ex
@@ -28,8 +28,9 @@ defmodule CodecovPhoenixExampleWeb do
 
   def view do
     quote do
-      use Phoenix.View, root: "lib/codecovPhoenixExample_web/templates",
-                        namespace: CodecovPhoenixExampleWeb
+      use Phoenix.View,
+        root: "lib/codecovPhoenixExample_web/templates",
+        namespace: CodecovPhoenixExampleWeb
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 2, view_module: 1]
